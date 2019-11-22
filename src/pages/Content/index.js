da70bcdb3f98ec45721dc14bfc1b9d50e726f7aa
Tomas Milar <milar@strands.com>
2019-11-22 15:53:54 -0300

    reformat code.

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -12,24 +12,24 @@ const pollUntilSentMessageReceived = () => {
   const maxTries = 30
   const interval = 2000
   let prevStatus = null
-  console.log("Start polling until sent message is received", {maxTries, interval})
+  console.log('Start polling until sent message is received', { maxTries, interval })
   const checkReceived = setInterval(() => {
-    const {message, status} = whatsapp.getLastSentMessage()
-    if(status !== prevStatus) {
-      sendUpdateNotification({status})
+    const { message, status } = whatsapp.getLastSentMessage()
+    if (status !== prevStatus) {
+      sendUpdateNotification({ status })
     }
-    if(status === "SENT_RECEIVED") {
-      console.log("Message received!")
+    if (status === 'SENT_RECEIVED') {
+      console.log('Message received!')
       clearInterval(checkReceived)
       // close tab???
       return
     }
-    console.log("Sent message status: ", status)
+    console.log('Sent message status: ', status)
     prevStatus = status
     tries++
-    if(tries === maxTries) {
+    if (tries === maxTries) {
       console.log(`Maximum attemps (${tries}) reached, abort. `)
-      sendUpdateNotification({status: "AFTER_SEND_ABORT"})
+      sendUpdateNotification({ status: 'AFTER_SEND_ABORT' })
       clearInterval(checkReceived)
     }
   }, interval)
@@ -39,23 +39,23 @@ const pollUntilMessageSent = () => {
   let tries = 0
   const maxTries = 30
   const interval = 2000
-  console.log("Start polling until message is sent",  {maxTries, interval})
+  console.log('Start polling until message is sent', { maxTries, interval })
 
   const autoSend = setInterval(() => {
     try {
       whatsapp.submitSendMessage()
-      console.log("Message sent!")
+      console.log('Message sent!')
       clearInterval(autoSend)
       pollUntilSentMessageReceived()
       return
-    } catch(error) {
-      console.log("Send message attempt failed, retrying. ", error)
+    } catch (error) {
+      console.log('Send message attempt failed, retrying. ', error)
     }
 
     tries++
-    if(tries === maxTries) {
+    if (tries === maxTries) {
       console.log(`Maximum attemps (${tries}) reached, abort. `)
-      sendUpdateNotification({status: "SEND_ABORT"})
+      sendUpdateNotification({ status: 'SEND_ABORT' })
       clearInterval(autoSend)
     }
 
