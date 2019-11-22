0603c280ac2f9283c8ab176f9fd262430bb71dc9
Tomas Milar <milar@strands.com>
2019-11-22 11:59:40 -0300

    Implement whatsap sender content script - polls whatsapp page until sending is done

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,6 +1,66 @@
-import { printLine } from './modules/print'
+import Whatsapp from './modules/whatsapp'
+const whatsapp = new Whatsapp()
 
-console.log('Content script works!')
-console.log('Must reload extension for modifications to take effect.')
+console.log('Content script loaded!')
 
-printLine('Using a function from the Print Module')
+function sendUpdateNotification({status}) {
+  chrome.runtime.sendMessage({type: "update-link", data: {status}})
+}
+
+const pollUntilSentMessageReceived = () => {
+  let tries = 0
+  const maxTries = 30
+  const interval = 2000
+  let prevStatus = null
+  console.log("Start polling until sent message is received", {maxTries, interval})
+  const checkReceived = setInterval(() => {
+    const {message, status} = whatsapp.getLastSentMessage()
+    if(status !== prevStatus) {
+      sendUpdateNotification({status})
+    }
+    if(status === "SENT_RECEIVED") {
+      console.log("Message received!")
+      clearInterval(checkReceived)
+      // close tab???
+      return
+    }
+    console.log("Sent message status: ", status)
+    prevStatus = status
+    tries++
+    if(tries === maxTries) {
+      console.log(`Maximum attemps (${tries}) reached, abort. `)
+      sendUpdateNotification({status: "AFTER_SEND_ABORT"})
+      clearInterval(checkReceived)
+    }
+  }, interval)
+}
+
+const pollUntilMessageSent = () => {
+  let tries = 0
+  const maxTries = 30
+  const interval = 2000
+  console.log("Start polling until message is sent",  {maxTries, interval})
+
+  const autoSend = setInterval(() => {
+    try {
+      whatsapp.submitSendMessage()
+      console.log("Message sent!")
+      clearInterval(autoSend)
+      pollUntilSentMessageReceived()
+      return
+    } catch(error) {
+      console.log("Send message attempt failed, retrying. ", error)
+    }
+
+    tries++
+    if(tries === maxTries) {
+      console.log(`Maximum attemps (${tries}) reached, abort. `)
+      sendUpdateNotification({status: "SEND_ABORT"})
+      clearInterval(autoSend)
+    }
+
+  }, interval)
+}
+
+console.log("Start polling send")
+pollUntilMessageSent()
---
+++ src/pages/Content/modules/whatsapp.js
@@ -0,0 +1,58 @@
+export default () => {
+  // helpers
+  const getOneElementByXpath = path =>
+    document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue
+
+  const _xPathResultsToArray = xPathResult => {
+    const nodes = []
+    let node = xPathResult.iterateNext()
+    while (node) {
+      nodes.push(node)
+      node = xPathResult.iterateNext()
+    }
+    return nodes
+  }
+
+  const getElementsByXpath = path => _xPathResultsToArray(document.evaluate(path, document, null, XPathResult.ANY_TYPE, null))
+
+  // parser
+  const getSubmitMsgButton = () => getOneElementByXpath('//*[@id="main"]/footer/div[1]/div[3]/button')
+
+  const getLastSentMessage = () => {
+    const sentMessages = getElementsByXpath('//*[@id="main"]//div[contains(@class, \'message-out\')]//span[contains(@class, \'selectable-text\')]/span')
+    if (!sentMessages || !sentMessages.length) {
+      return null
+    }
+    const { innerText: message } = sentMessages.pop()
+
+    const sentMessagesStatuses = getElementsByXpath('//*[@id="main"]//div[contains(@class, \'message-out\')]/div[1]/div[1]/div[1]/div[2]//span[@data-icon]')
+    const lastSentMessageStatus = sentMessagesStatuses.pop()
+    const statusIcon = lastSentMessageStatus.getAttribute('data-icon')
+    const statusIcons = {
+      'msg-dblcheck': 'SENT_RECEIVED',
+      'msg-check': 'SENT',
+      'msg-time': 'WAITING',
+    }
+    const status = statusIcons[statusIcon]
+
+    return {
+      message,
+      status,
+    }
+  }
+
+  // methods
+  const submitSendMessage = () => {
+    const submitBtn = getSubmitMsgButton()
+    if (!submitBtn) {
+      throw new Error('No submit button found')
+    }
+    console.log('Submitted message')
+    submitBtn.click()
+  }
+
+  return {
+    submitSendMessage,
+    getLastSentMessage,
+  }
+}
